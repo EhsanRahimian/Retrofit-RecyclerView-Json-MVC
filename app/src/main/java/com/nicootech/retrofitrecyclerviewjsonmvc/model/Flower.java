1b2ba6c175fbbd433dc525bdf66c9be960203e96
Ehsan <ehrath1@gmail.com>
2019-08-17 12:17:14 -0700

    Flower is our pojo class to hold the information of each flower our json url is: http://services.hanselandpetal.com/feeds/flowers.json

---
+++ app/src/main/java/com/nicootech/retrofitrecyclerviewjsonmvc/model/Flower.java
@@ -0,0 +1,5 @@
+package com.nicootech.retrofitrecyclerviewjsonmvc.model;
+
+public class Flower {
+
+}
--- app/src/main/java/com/nicootech/retrofitrecyclerviewjsonmvc/view/MainActivity.java
+++ app/src/main/java/com/nicootech/retrofitrecyclerviewjsonmvc/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.nicootech.retrofitrecyclerviewjsonmvc;
+package com.nicootech.retrofitrecyclerviewjsonmvc.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.nicootech.retrofitrecyclerviewjsonmvc.R;
 
 public class MainActivity extends AppCompatActivity {
 
